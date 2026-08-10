2f9d9006cf983bcf516a1f70a65321b079a2d07d
Akilshoev Firdavs <kinglook600@gmail.com>
2026-08-10 22:27:54 +0500

    Changed comenters file

--- src/app/DataBase/comenters.ts
+++ src/app/DataBase/comenters.ts
@@ -2,7 +2,7 @@ import type { IFormCommenters} from "../components/UI/Cards/ComentatersCard/lib/
 
 const comenters: IFormCommenters[] =  [
     {id: 1, image: "public/image/comenters.png", job: "ООО ИТ Консалтинг", name: "Логинова Анастасия", icon: "/icon/pero.png", comment: "Многие думают, что Lorem Ipsum — взятый с потолка псевдо-латинский набор слов, но это не совсем так. Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад."},
-    {id: 2, image: "public/image/comenter.png", job: "АО ПРОМАРШ, name:", name: "Макаров Сергей", icon: "/icon/pero.png", comment: "Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите какой-нибудь шутки, скрытой в середине абзаца."}
+    {id: 2, image: "public/image/comenter.png", job: "АО ПРОМАРШ", name: "Макаров Сергей", icon: "/icon/pero.png", comment: "Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите какой-нибудь шутки, скрытой в середине абзаца."}
 ]
 
 export default comenters
