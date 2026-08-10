9508c97df52eca6082c8d704d622f513e67f90a2
Akilshoev Firdavs <kinglook600@gmail.com>
2026-08-10 10:02:44 +0300

    Created a new file buttons for slider

---
+++ src/app/components/Home/components/HomeButtonsSlider/HomeButttonsSlider.tsx
@@ -0,0 +1,12 @@
+
+
+const HomeButttonsSlider = () => {
+  return (
+     <div className="relative flex justify-between mx-20 items-center">
+      <button><img className=" w-10" src="public/icon/prev.png" alt="" /></button>
+      <button><img className=" w-10" src="public/icon/right.png" alt="" /></button>
+    </div>
+  )
+}
+
+export default HomeButttonsSlider
