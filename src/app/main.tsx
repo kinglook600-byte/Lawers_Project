07a3ef701bf967dccc93a56f989eabfee136e3d4
Akilshoev Firdavs <kinglook600@gmail.com>
2026-08-11 08:57:57 +0300

    added deleted i18n

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "../i18n";
 
 import router from "../app/routes/routes";
 
