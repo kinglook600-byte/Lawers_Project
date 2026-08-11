f0025ef2603cb4fbdc51539a74cc360d75f665fd
Akilshoev Firdavs <kinglook600@gmail.com>
2026-08-11 07:10:35 +0300

    Chenged component

--- src/app/components/Home/components/ComentatersSection/HomeCommentatersCardSection.tsx
+++ src/app/components/Home/components/ComentatersSection/HomeCommentatersCardSection.tsx
@@ -1,13 +1,15 @@
 import ComentatersCard from "../../../UI/Cards/ComentatersCard/ComentatersCard";
 import comentaters from "../../../../DataBase/comenters";
+import { useTranslation } from 'react-i18next'
 
 const HomeCommentatersCardSection = () => {
+  const { t } = useTranslation()
   return (
     <div>
       <div className="mx-2 lg:mx-10 2xl:mx-27">
         <div className="flex justify-center items-center flex-col mb-10 2xl:justify-between">
           <h2 className="text-[18px] sm:text-[20px] md:text-[24px] lg:text-[20px]  2xl:text-[25px]">
-            Что о нас говорят клиенты
+            {t('key30', 'Что о нас говорят клиенты')}
           </h2>
         </div>
       </div>
--- src/app/components/Home/components/HomeImportantMaterialSection/HomeImportantMaterialSection.tsx
+++ src/app/components/Home/components/HomeImportantMaterialSection/HomeImportantMaterialSection.tsx
@@ -1,20 +1,20 @@
 
 import importantMaterial from "../../../../DataBase/importantMaterial";
 import ImportantMaterial from "../../../UI/Cards/ImportantMaterialCard/ImportantMaterialCard";
+import { useTranslation } from 'react-i18next'
 // import HomeButttonsSlider from "../HomeButtonsSlider/HomeButttonsSlider";
 
 const HomeImportantMaterialSection = () => {
+  const { t } = useTranslation()
   return (
     <div>
       <div className=" flex justify-center flex-col items-center">
         <h1 className="text-[20px] text-center 2xl:text-[30px]">
-          Полезные материалы
+          {t('key18', 'Полезные материалы')}
         </h1>
 
         <p className=" mt-4 mb-5 text-center mx-20 text-[16px] md:text-[20px] 2xl:max-w-4xl 2xl:mb-10 ">
-          Давно выяснено, что при оценке дизайна и композиции читаемый текст
-          мешает сосредоточиться. Lorem Ipsum используют потому, что тот
-          обеспечивает более или менее стандартное заполнение шаблона.
+          {t('loremIpsum5', 'Давно выяснено, что при оценке дизайна и композиции читаемый текст\r\n          мешает сосредоточиться. Lorem Ipsum используют потому, что тот\r\n          обеспечивает более или менее стандартное заполнение шаблона.')}
         </p>
       </div>
       <div className="mx-3 lg:grid grid-cols-2 lg:grid-cols-2 xl:flex ">
