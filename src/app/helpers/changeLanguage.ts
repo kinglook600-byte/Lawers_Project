348c811f0728b0ca9728dfe03d6d8ab125a5a1b6
Akilshoev Firdavs <kinglook600@gmail.com>
2026-08-11 12:08:43 +0300

    Commited code

---
+++ src/app/helpers/changeLanguage.ts
@@ -0,0 +1,9 @@
+
+
+
+// const handleLanguage = (i18n: any) => {
+//     const newLanguage = i18n.language === "ru" ? "en" : "ru"
+//     i18n.changeLanguage(newLanguage)
+//   }
+
+// export default handleLanguage
