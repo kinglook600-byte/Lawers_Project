a2f396d573a6ea6e2e3c8f1ccc42de1988fd3f91
Akilshoev Firdavs <kinglook600@gmail.com>
2026-08-11 07:11:30 +0300

    add words for translation

---
+++ src/i18n.ts
@@ -0,0 +1,16 @@
+
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import resourcesToBackend from 'i18next-resources-to-backend'
+
+i18next
+  .use(initReactI18next)
+  .use(resourcesToBackend((language: string) => import(`./locales/${language}/translation.json`)))
+  .init({
+    returnEmptyString: false,
+
+    fallbackLng: 'en',
+    defaultNS: 'translation',
+  })
+
+export default i18next
