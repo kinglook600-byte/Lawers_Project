adeae3302bf01e0f4c432422ce5bd39d0781432e
Akilshoev Firdavs <kinglook600@gmail.com>
2026-08-11 22:47:51 +0500

    Changed DataBase files

--- src/app/DataBase/card.ts
+++ src/app/DataBase/card.ts
@@ -1,45 +1,42 @@
-// import { useTranslation } from "react-i18next"
-import type { ICardBlock } from "../components/UI/Cards/BlockCard/lib/types/typeCardBlock"
-
-
+import type { ICardBlock } from "../components/UI/Cards/BlockCard/lib/types/typeCardBlock";
 
 const card: ICardBlock[] = [
-    {
-      id: 1,
-      icon: "public/icon/agreement.png",
-      title: "Банкротство физических лиц",
-      text: "Процедуру банкротства физических лиц в России регулирует закон.",
-    },
-    {
-      id: 2,
-      icon: "public/icon/agreement.png",
-      title: "Сопровождение сделок",
-      text: "Инициирование, оформление, осуществление сделки и достижение результата — это именно то, чем мы занимаемся каждый день.",
-    },
-    {
-      id: 3,
-      icon: "public/icon/agreement.png",
-      title: "Корпоративные споры",
-      text: "Корпоративные споры — споры, связанные с созданием юридического лица, управлением или участием в нем.",
-    },
-    {
-      id: 4,
-      icon: "public/icon/agreement.png",
-      title: "Недвижимость и строительство",
-      text: "Консультируем по различным вопросам в области недвижимости, включая градостроительное регулирование и земельные отношения.",
-    },
-    {
-      id: 5,
-      icon: "public/icon/agreement.png",
-      title: "Возврат страховых премий",
-      text: "Для возврата страховой премии необходимо подготовить паспорт страхователя и оригинал страхового полиса.",
-    },
-    {
-      id: 6,
-      icon: "public/icon/agreement.png",
-      title: "Налоговое право и споры",
-      text: "Сопровождение налоговых проверок на любой стадии, анализ документов и защита при налоговых спорах.",
-    }
-  ]
+  {
+    id: 1,
+    icon: "public/icon/agreement.png",
+    title: "BlockCard.1.title",
+    text: "BlockCard.1.text",
+  },
+  {
+    id: 2,
+    icon: "public/icon/agreement.png",
+    title: "BlockCard.2.title",
+    text: "BlockCard.2.text",
+  },
+  {
+    id: 3,
+    icon: "public/icon/agreement.png",
+    title: "BlockCard.3.title",
+    text: "BlockCard.3.text",
+  },
+  {
+    id: 4,
+    icon: "public/icon/agreement.png",
+    title: "BlockCard.4.title",
+    text: "BlockCard.4.text",
+  },
+  {
+    id: 5,
+    icon: "public/icon/agreement.png",
+    title: "BlockCard.5.title",
+    text: "BlockCard.5.text",
+  },
+  {
+    id: 6,
+    icon: "public/icon/agreement.png",
+    title: "BlockCard.6.title",
+    text: "BlockCard.6.text",
+  },
+];
 
- export default card 
+export default card;
--- src/app/DataBase/comenters.ts
+++ src/app/DataBase/comenters.ts
@@ -1,8 +1,8 @@
-import type { IFormCommenters} from "../components/UI/Cards/ComentatersCard/lib/types/typeComentatersCard"
+import type { IFormCommenters } from "../components/UI/Cards/ComentatersCard/lib/types/typeComentatersCard"
 
-const comenters: IFormCommenters[] =  [
-    {id: 1, image: "public/image/comenters.png", job: "ООО ИТ Консалтинг", name: "Логинова Анастасия", icon: "/icon/pero.png", comment: "Многие думают, что Lorem Ipsum — взятый с потолка псевдо-латинский набор слов, но это не совсем так. Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад."},
-    {id: 2, image: "public/image/comenter.png", job: "АО “ПРОМАРШ”", name: "Макаров Сергей", icon: "/icon/pero.png", comment: "Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите какой-нибудь шутки, скрытой в середине абзаца."}
+const comenters: IFormCommenters[] = [
+    { id: 1, image: "public/image/comenters.png", job: "ComentersCard.1.job", name: "ComentersCard.1.name", icon: "/icon/pero.png", comment: "ComentersCard.1.comment" },
+    { id: 2, image: "public/image/comenter.png", job: "ComentersCard.2.job", name: "ComentersCard.2.name", icon: "/icon/pero.png", comment: "ComentersCard.2.comment" },
 ]
 
 export default comenters
