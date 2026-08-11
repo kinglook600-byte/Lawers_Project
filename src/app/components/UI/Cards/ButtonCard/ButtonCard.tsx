25d09742418d3e409bd7ee93aa35958b8ce66133
Akilshoev Firdavs <kinglook600@gmail.com>
2026-08-11 22:49:48 +0500

    Changed components

--- src/app/components/UI/Cards/ButtonCard/ButtonCard.tsx
+++ src/app/components/UI/Cards/ButtonCard/ButtonCard.tsx
@@ -5,9 +5,9 @@ const Button = () => {
   const { t } = useTranslation()
   return (
     <div>
-         <button className="w-40 bg-blue-500 mx-5 rounded-[5px] sm:mx-13 md:mx-23 lg:bg-blue-500 animate-bounce my-10 p-4 lg:w-50 text-white 2xl: py-3 2xl:rounded-lg 2xl:mx-25 2xl:mt-10 ">
-          {t('ButtonCard', 'Все новости')}
-        </button>
+      <button className="w-40 bg-blue-500 mx-5 rounded-[5px] sm:mx-13 md:mx-23 lg:bg-blue-500 animate-bounce my-10 p-4 lg:w-50 text-white 2xl: py-3 2xl:rounded-lg 2xl:mx-25 2xl:mt-10 ">
+        {t('ButtonCard')}
+      </button>
     </div>
   )
 }
