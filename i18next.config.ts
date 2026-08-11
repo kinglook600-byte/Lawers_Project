2789885bc03793e359184fc91d74535af8fc4f42
Akilshoev Firdavs <kinglook600@gmail.com>
2026-08-11 08:56:25 +0300

    I put it on commit

--- i18next.config.ts
+++ i18next.config.ts
@@ -1,15 +1,16 @@
-import en from "./src/app/Locales/en/translation.json";
-import ru from "./src/app/Locales/ru/translation.json";
-import tj from "./src/app/Locales/tj/translation.json";
+// import en from "./src/app/Locales/en/translation.json";
+// import ru from "./src/app/Locales/ru/translation.json";
+// // import tj from "./src/app/Locales/tj/translation.json";
 
-export const resources = {
-  en: {
-    translation: en,
-  },
-  ru: {
-    translation: ru,
-  },
-  tj: {
-    translation: tj,
-  },
-} as const;
+// export const resources = {
+//   lng: en,
+//   en: {
+//     translation: en,
+//   },
+//   ru: {
+//     translation: ru,
+//   },
+//   // tj: {
+//   //   translation: tj,
+//   // },
+// } as const;
