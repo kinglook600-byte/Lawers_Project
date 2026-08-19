2f0b349388b84feb04a2ea7793250c20ea377cd5
Akilshoev Firdavs <kinglook600@gmail.com>
2026-08-19 15:09:27 +0300

    Commited and deleted file

--- src/app/helpers/changeLanguage.ts
+++
@@ -1,9 +0,0 @@
-
-
-
-// const handleLanguage = (i18n: any) => {
-//     const newLanguage = i18n.language === "ru" ? "en" : "ru"
-//     i18n.changeLanguage(newLanguage)
-//   }
-
-// export default handleLanguage
